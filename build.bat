a9dbcd9c41bc538c796fbb2170abe3e38f90e533
rommmmaha <rommmmaha@gmail.com>
2025-06-08 14:02:01 +0300

    bruh

--- build.bat
+++ build.bat
@@ -1,20 +1,21 @@
 @echo off
 SET "ROOT=%~dp0"
-IF exist "%ROOT%!ratus-production" (
-  rd /s /q "%ROOT%!ratus-production"
+echo ROOT is %ROOT%
+IF exist "%ROOT%ratus-build" (
+  rd /s /q "%ROOT%ratus-build"
 )
 
-mkdir "%ROOT%!ratus-production"
-mkdir "%ROOT%!ratus-production\admin"
+mkdir "%ROOT%ratus-build"
+mkdir "%ROOT%ratus-build\admin"
 
 cd "%ROOT%ratus-server"
-CALL bun build ./src/index.ts --compile --outfile "%ROOT%!ratus-production\server.exe"
-copy /Y "%ROOT%ratus-server\passkeys.json" "%ROOT%!ratus-production\passkeys.json"
+CALL bun build ./src/index.ts --compile --outfile "%ROOT%ratus-build\server.exe"
+copy /Y "%ROOT%ratus-server\passkeys.json" "%ROOT%ratus-build\passkeys.json"
 
 cd "%ROOT%ratus-admin"
 CALL bun run build
-xcopy /E /I /Y dist "%ROOT%!ratus-production\admin\"
+xcopy /E /I /Y dist "%ROOT%ratus-build\admin\"
 
 cd "%ROOT%ratus-client-rust"
 CALL cargo build --release
-copy /Y target\release\ratus-client-rust.exe "%ROOT%!ratus-production\client-rust.exe"
+copy /Y target\release\ratus-client-rust.exe "%ROOT%ratus-build\client-rust.exe"
